a42e68cf08741595e55548dc27b0a12fcfc161b5
ineptErick <inepterick@gmail.com>
2023-07-18 22:25:20 +0300

    рефакторинг

--- stats/stats-client/src/main/java/ru/practicum/StatisticClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatisticClient.java
@@ -8,55 +8,44 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-import ru.practicum.dto.StatisticPostDto;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import static org.springframework.http.RequestEntity.post;
 
 @Service
 public class StatisticClient {
+
+    private String serverUrl = "http://localhost:9090";
+
     private final RestTemplate rest =  new RestTemplateBuilder()
-            .uriTemplateHandler(new DefaultUriBuilderFactory("${server.url}"))
+            .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
             .requestFactory(HttpComponentsClientHttpRequestFactory::new)
             .build();
 
     protected  <T> ResponseEntity<Object> post(String path, T body) {
-        return makeAndSendRequest(HttpMethod.POST, path, null, body);
+        return makeAndSendRequest(HttpMethod.POST, serverUrl + path, body);
     }
 
     protected ResponseEntity<Object> get(String path, String start, String end, String[] uris, Boolean isUnique) {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("start", start);
-        parameters.put("end", end);
-        parameters.put("isUnique", isUnique);
-        if (uris != null) {
-            parameters.put("uris", uris);
-        }
-        StringBuilder fullPath = new StringBuilder().append(path);
+        StringBuilder fullPath = new StringBuilder().append(serverUrl).append(path);
         fullPath.append("?start=").append(start).append("&end=").append(end);
         if (uris != null){
             for (String uri: uris) {
                 fullPath.append("&uris=").append(uri);
             }
         }
+        System.out.println(rest.getUriTemplateHandler());
         fullPath.append("&isUnique=").append(isUnique);
-        return makeAndSendRequest(HttpMethod.GET, fullPath.toString(), parameters, null);
+        return makeAndSendRequest(HttpMethod.GET, fullPath.toString(), null);
     }
 
     private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path,
-                                                          @Nullable Map<String, Object> parameters, @Nullable T body) {
+                                                           @Nullable T body) {
         HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
 
         ResponseEntity<Object> exploreWithMeServerResponse;
         try {
-            if (parameters != null) {
-                exploreWithMeServerResponse = rest.exchange(path, method, requestEntity, Object.class, parameters);
-            } else {
                 exploreWithMeServerResponse = rest.exchange(path, method, requestEntity, Object.class);
-            }
         } catch (HttpStatusCodeException e) {
             return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
         }
