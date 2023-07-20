ef56f602147a1d1b40d964b410ca83adecb96b78
ineptErick <inepterick@gmail.com>
2023-07-21 00:50:59 +0300

    несколько правок done

--- stats/stats-server/src/main/java/ru/practicum/model/Statistic.java
+++ stats/stats-server/src/main/java/ru/practicum/model/Statistic.java
@@ -1,13 +1,22 @@
 package ru.practicum.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+// вред аннотации data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "statistic")
-@Data
 public class Statistic {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +33,21 @@ public class Statistic {
 
     @Column(name = "view_date", nullable = false)
     private LocalDateTime timestamp;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        Statistic statistic = (Statistic) o;
+        return id != null && Objects.equals(id, statistic.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- stats/stats-server/src/main/java/ru/practicum/repository/StatisticRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/StatisticRepository.java
@@ -1,15 +1,14 @@
 package ru.practicum.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.practicum.dto.StatisticGetDto;
 import ru.practicum.model.Statistic;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Component("dbStatisticRepository")
+@Repository("dbStatisticRepository")
 public interface StatisticRepository  extends JpaRepository<Statistic, Long> {
 
     //Просмотры всех uri (не уникальные)
